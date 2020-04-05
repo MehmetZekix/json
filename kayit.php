d93194cabdb4290ef4244ce1dd2a3a2971f68bc2
Mehmet Zeki <mehzer@gmail.com>
2020-04-05 11:36:51 +0300

    Add files via upload

---
+++ kayit.php
@@ -0,0 +1,14 @@
+<?php
+if($_POST){
+
+$city = $POST['city'];
+$country = $POST['country'];
+$email = $POST['email'];
+$nameSurname = $POST['nameSurname'];
+$password = $POST['password'];
+
+
+echo $email;
+}
+
+?>
